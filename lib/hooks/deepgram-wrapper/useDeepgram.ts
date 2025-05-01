0b31220fad0cdbe13421d33aae78cb7ad8462b83
JuanCSUCoder <jcsu2019369@gmail.com>
2025-05-01 16:14:04 -0500

    Feat: Implements deepgram hook

---
+++ lib/hooks/deepgram-wrapper/useDeepgram.ts
@@ -0,0 +1,39 @@
+import { createClient, LiveClient, LiveSchema, LiveTranscriptionEvents, SOCKET_STATES } from "@deepgram/sdk";
+import { useState } from "react";
+
+export const useDeepgram = () => {
+  const [connection, setConnection] = useState<LiveClient | null>(null);
+  const [connectionState, setConnectionState] = useState<SOCKET_STATES>(SOCKET_STATES.closed);
+
+  const connectToDeepgram = async (options: LiveSchema) => {
+    const key = process.env.DEEPGRAM_API_KEY;
+    const endpoint = process.env.DEEPGRAM_API_ENDPOINT || "";
+    const deepgram = createClient(key);
+
+    const conn = deepgram.listen.live(options, endpoint);
+
+    conn.addListener(LiveTranscriptionEvents.Open, () => {
+      setConnectionState(SOCKET_STATES.open);
+    });
+
+    conn.addListener(LiveTranscriptionEvents.Close, () => {
+      setConnectionState(SOCKET_STATES.closed);
+    });
+
+    setConnection(conn);
+  };
+
+  const disconnectFromDeepgram = async () => {
+    if (connection) {
+      connection.requestClose();
+      setConnection(null);
+    }
+  };
+
+  return {
+    connectToDeepgram,
+    disconnectFromDeepgram,
+    connection,
+    connectionState,
+  }
+};
