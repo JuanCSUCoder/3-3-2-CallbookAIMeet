125fd65f4d1b0aa299afd52fde96bab45beba21e
JuanCSUCoder <jcsu2019369@gmail.com>
2025-05-01 15:01:30 -0500

    Feat: Connects audio stream sources

--- lib/hooks/audio-stream/useAudioProcessor.ts
+++ lib/hooks/audio-stream/useAudioProcessor.ts
@@ -7,20 +7,15 @@ export const useAudioProcessor = (room: Room) => {
   console.log("STT.Processor - Processing")
 
   const [streams, audioContext] = useAudio(room);
-  const [pcmWorklet, setWorklet] = useState<AudioWorkletNode | null>(null);
 
+  const [pcmWorklet, setWorklet] = useState<AudioWorkletNode | null>(null);
+  const [sources, setSources] = useState<MediaStreamAudioSourceNode[]>([]);
+
+  // 1. Inicializa el procesador de audio: AudioWorklet
   useEffect(() => {
     console.log("STT - Audio info", streams, audioContext);
 
-    if (audioContext && streams.length > 0) {
-      for (let i = 0; i < streams.length; i++) {
-        const stream = streams[i];
-
-        const mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-        mediaStreamSource.connect(audioContext.destination);
-      }
-
+    if (audioContext) {
       audioContext.audioWorklet.addModule('audio-pcm-worklet.js')
         .then((module) => {
           console.log("STT.Processor - Module Added", module)
@@ -29,7 +24,30 @@ export const useAudioProcessor = (room: Room) => {
           return audioContext.resume()
         }).then(res => console.log("STT.Processor - Ready", res));
     }
-  }, [streams, audioContext]);
+  }, [audioContext]);
+
+  // 2. Conecta el AudioWorklet a los streams de audio
+  useEffect(() => {
+    if (audioContext && pcmWorklet) {
+      for (let i = 0; i < streams.length; i++) {
+        const stream = streams[i];
+
+        // 1. Crea el MediaStreamSource
+        const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+        // 2. Conecta el MediaStreamSource al AudioWorklet
+        mediaStreamSource.connect(pcmWorklet);
+
+        // 3. Conecta el AudioWorklet al destino de audio
+        pcmWorklet.connect(audioContext.destination);
+
+        setSources((prev) => {
+          const newSources = [...prev, mediaStreamSource];
+          return newSources;
+        });
+      }
+    }
+  }, [streams, audioContext, pcmWorklet]);
 
   return pcmWorklet;
 };
